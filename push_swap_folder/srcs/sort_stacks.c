01aecb60259b2ff3bee86197e8b1659b46035ae2
Marco Nunnari <mnunnari@student.42.us.org>
2017-06-02 19:20:52 +0200

    ok

--- push_swap_folder/srcs/sort_stacks.c
+++ push_swap_folder/srcs/sort_stacks.c
@@ -6,7 +6,7 @@
 /*   By: mnunnari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/02 14:52:36 by mnunnari          #+#    #+#             */
-/*   Updated: 2017/06/02 18:49:23 by mnunnari         ###   ########.fr       */
+/*   Updated: 2017/06/02 19:14:40 by mnunnari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,52 +45,35 @@ void			sort_a3(t_list **a)
 		reverse_rotate_a(a, NULL);
 }
 
-void			sort_a4(t_list **a, t_list **b)
+void			sort_a(t_list **a, t_list **b, int len_a)
 {
 	t_list	*st;
-	int		bottom;
-	int		middle1;
-	int		middle2;
-	int		top;
+	int		min;
+	int		i_min;
+	int		i;
 
+	if (stack_sorted(*a))
+		return ;
 	st = *a;
-	top = nbr(st->next->next->next);
-	middle1 = nbr(st->next->next);
-	middle2 = nbr(st->next);
-	bottom = nbr(st);
-	if (top < middle1 && top < middle2 && top < bottom)
+	min = nbr(st);
+	i_min = 0;
+	i= 0;
+	while (st)
+	{
+		if (nbr(st) < min)
+		{
+			min = nbr(st);
+			i_min = i;
+		}
+		i++;
+		st = st->next;
+	}
+	if (i_min == len_a - 1)
 		push_b(a, b);
-	else if (middle1 < top && middle1 < middle2 && middle1 < bottom)
+	else if (i_min >= len_a / 2)
 		rotate_a(a, b);
-	else if (bottom < top && bottom < middle1 && bottom < middle2)
+	else
 		reverse_rotate_a(a, b);
-	else if (middle2 < top && middle2 < middle1 && middle2 < bottom)
-		rotate_a(a, b);
-}
-
-void			sort_a5(t_list **a, t_list **b)
-{
-	t_list	*st;
-	int		bottom;
-	int		middle1;
-	int		middle2;
-	int		middle3;
-	int		top;
-
-	st = *a;
-	top = nbr(st->next->next->next->next);
-	middle1 = nbr(st->next->next->next);
-	middle2 = nbr(st->next->next);
-	middle3 = nbr(st->next);
-	bottom = nbr(st);
-	if (top < middle1 && top < middle2 && top < bottom && top < middle3)
-		push_b(a, b);
-	if (middle1 < top && middle1 < middle2 && middle1 < bottom && middle1 < middle3)
-		rotate_a(a, b);
-	if (bottom < top && bottom < middle1 && bottom < middle2 && bottom < middle3)
-		reverse_rotate_a(a, b);
-	if (middle2 < top && middle2 < middle1 && middle2 < bottom)
-		rotate_a(a, b);
 }
 
 size_t			ft_lstlen(t_list *lst)
@@ -119,9 +102,9 @@ void			sort_stacks(t_list **a, t_list **b)
 			sort_a2(a);
 		else if (len_a == 3)
 			sort_a3(a);
-		else if (len_a == 4)
-			sort_a4(a, b);
-		if (len_b == 1 && stack_sorted(*a))
+		else
+			sort_a(a, b, len_a);
+		if (len_b > 0 && stack_sorted(*a))
 			push_a(a, b);
 	}
 }
